0dc0c343bdf70ad9764bbc32d7ab2608a98c9227
ismail Shuaau <ismailshuaau@gmail.com>
2018-03-17 09:46:46 +0500

    timestamp added

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 https://timestamp-ms.herokuapp.com/December%2015,%202015
 https://timestamp-ms.herokuapp.com/1450137600
 ```
-Example output:
+### Example output:
 ```
 { "unix": 1450137600, "natural": "December 15, 2015" }
 ```
--- app.js
+++ app.js
@@ -8,6 +8,28 @@ var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// GET Call to return JSON that formats natural and unix date
+app.get('/dateValues/:dateVal', function(req,res,next) {
+  var dateVal = req.params.dateVal;
+  // Options for formatting date in natural date time
+  var dateFomattingOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+
+  if(isNaN(dateVal)) {
+    var naturalDate = new Date(dateVal);
+    naturalDate = naturalDate.toLocaleDateString('en-us', dateFomattingOptions);
+    var unixDate = new Date(dateVal).getTime()/1000;
+  } else {
+    var unixDate = dateVal;
+    var naturalDate = new Date(dateVal * 1000);
+    naturalDate = naturalDate.toLocaleDateString('en-us', dateFomattingOptions);
+  }
+  res.json({unix: unixDate, natural: naturalDate});
+});
+
 app.listen(3000, function(){
   console.log("It's Working");
 })
